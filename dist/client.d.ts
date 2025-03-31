14e7f1ea0b9046a5e4c6d4a7c0d6fa0a3b0d07fd
retz8 <ekkicb71@gmail.com>
2025-03-31 17:31:01 -0400

    .

--- dist/client.d.ts
+++
@@ -1,2 +0,0 @@
-export declare const client: import("axios").AxiosInstance;
-//# sourceMappingURL=client.d.ts.map
--- dist/types.js
+++
@@ -1,9 +0,0 @@
-var OrderStatus;
-(function (OrderStatus) {
-    OrderStatus["PENDING"] = "pending";
-    OrderStatus["PREPARING"] = "preparing";
-    OrderStatus["READY"] = "ready";
-    OrderStatus["CLOSED"] = "closed";
-})(OrderStatus || (OrderStatus = {}));
-export { OrderStatus };
-//# sourceMappingURL=types.js.map
